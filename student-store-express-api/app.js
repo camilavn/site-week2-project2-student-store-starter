6cbf763df6a1333d9f40ca9f40cdf8e117d6aee5
Camila Naranjo <cnaranjo@cnaranj-ltm6e77.internal.salesforce.com>
2023-06-21 11:00:16 -0700

    added backend

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,17 @@
-// YOUR CODE HERE
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const db = require('./data/db.json');
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200);
+    res.send(db);
+});
+
+module.exports = app;
